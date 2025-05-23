da32633fb2ad75e78568a8b4d96d043fbf39d533
Gaeon <gaeon0413@gmail.com>
2025-05-23 23:20:47 +0900

    feat : 디스플레이 기준에 따라 갤러리뷰/리스트뷰로 볼 수 있도록 수정

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,63 @@
+<template>
+	<table class="project-table">
+	  <thead>
+		<tr>
+		  <th>제목</th>
+		  <th>소스</th>
+		  <th>생성됨</th>
+		</tr>
+	  </thead>
+	  <tbody>
+		<tr v-for="project in projects" :key="project.id">
+		  <td class="title-cell">
+			{{ project.name }}
+		  </td>
+		  <td>{{ project.versionInfo }}</td>
+		  <td>{{ project.date }}</td>
+		</tr>
+	  </tbody>
+	</table>
+  </template>
+  
+  <script setup>
+  const props = defineProps({
+	projects: {
+	  type: Array,
+	  required: true
+	}
+  });
+  </script>
+  
+  <style scoped>
+  .project-table {
+	width: 100%;
+	border-collapse: collapse;
+	color: #333;
+  }
+  
+  .project-table th,
+  .project-table td {
+	padding: 16px;
+	text-align: left;
+	border-bottom: 1px solid #444;
+  }
+  
+  .project-table th {
+	color: #aaa;
+	font-weight: normal;
+  }
+  
+  .title-cell {
+	display: flex;
+	align-items: center;
+  }
+  
+  .thumbnail {
+	width: 28px;
+	height: 28px;
+	margin-right: 10px;
+	border-radius: 4px;
+	object-fit: cover;
+  }
+  </style>
+  
