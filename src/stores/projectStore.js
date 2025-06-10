16c955b261eb76adc70889952c41b24c397342a0
Gaeon <gaeon0413@gmail.com>
2025-06-10 11:05:15 +0900

    초기화면 생성

--- src/stores/projectStore.js
+++ src/stores/projectStore.js
@@ -4,14 +4,18 @@ export const useProjectStore = defineStore('projectStore', {
     state: () => ({
         projectId: null,
         projectName: null,
+        // 추후 수정
+        // Project 상태 저장 (요구사항 입력 전 / 요구사항 생성중 / 요구사항 생성완료)
+        projectRevision: null,
         status: null,
         userId: null,
         isAdmin: null
     }),
     actions: {
-        setProject(projectId, projectName, status, isAdmin) {
+        setProject(projectId, projectName, projectRevision, status, isAdmin) {
             this.projectId = projectId;
             this.projectName = projectName;
+            this.projectRevision = projectRevision;
             this.status = status;
             this.isAdmin = isAdmin;
         },
@@ -21,6 +25,7 @@ export const useProjectStore = defineStore('projectStore', {
         resetProject() {
             this.projectId = null;
             this.projectName = null;
+            this.projectRevision = null;
             this.status = null;
             this.isAdmin = null;
         },
@@ -28,6 +33,7 @@ export const useProjectStore = defineStore('projectStore', {
             this.userId = null;
             this.projectId = null;
             this.projectName = null;
+            this.projectRevision = null;
             this.status = null;
             this.isAdmin = null;
         },
