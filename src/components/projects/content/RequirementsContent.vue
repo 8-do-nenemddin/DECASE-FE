98e365a401662af424668d45b2ec4cb1dfb1f9fb
Gaeon <gaeon0413@gmail.com>
2025-06-11 19:53:00 +0900

    detail 입력창 막기/임티 수정

--- src/components/projects/content/RequirementsContent.vue
+++ src/components/projects/content/RequirementsContent.vue
@@ -3,7 +3,28 @@
     <div class="table-container">
       <div class="header-section">
         <div class="content-info">
-          <h2 class="content-title">📋 요구사항 정의서</h2>
+          <h2 class="content-title">
+            <span>
+
+            </span>
+            <svg
+                width="23"
+                height="23"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+            >
+              <path
+                  d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"
+              ></path>
+              <polyline points="14 2 14 8 20 8"></polyline>
+              <line x1="16" y1="13" x2="8" y2="13"></line>
+              <line x1="16" y1="17" x2="8" y2="17"></line>
+              <polyline points="10 9 9 9 8 9"></polyline>
+            </svg>
+            <span> 요구사항 정의서 </span>
+          </h2>
           <div class="file-details">
             <span class="detail-item">ver. {{ revision }}</span>
           </div>
