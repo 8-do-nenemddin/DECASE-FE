9f622764935409fcde7fac715e48c78e86e17b3a
hyoJ <goodforx0147@naver.com>
2025-06-25 15:19:53 +0900

    fix: 경로 수정

--- src/components/projects/content/SrsFailedContent.vue
+++ src/components/projects/content/SrsFailedContent.vue
@@ -2,7 +2,7 @@
   <div class="failed-card">
     <h2>요구사항 정의서 생성 실패</h2>
     <p>요구사항 정의서 생성에 실패했습니다.<br />다시 시도해 주세요.</p>
-    <button @click="goToBasicContent">재시도</button>
+    <button @click="goToBasicContent">돌아가기</button>
     <div v-if="message" class="error-message">{{ message }}</div>
   </div>
 </template>
@@ -12,7 +12,7 @@ import { useRouter } from 'vue-router';
 const router = useRouter();
 
 const goToBasicContent= () => {
-  router.push('/basic');
+  window.location.reload();
 };
 
 
